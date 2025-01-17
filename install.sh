51139f1ddc62a89c68465f6c0149492e04a403ee
alex <alekseivoronavost@gmail.com>
2025-01-18 00:30:15 +0300

    add 5

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-base_python_interpreter="python3.12.6"
+base_python_interpreter="/usr/bin/python3.10"
 project_domain="alexvtyk.beget.tech"
-project_path="C:\Users\1\drf2"
+project_path="/home/a/alexvtyk/dr2"
 
 read -p "Python interpreter: " base_python_interpreter
 read -p "Your domain without protocol (for example, google.com): " project_domain
@@ -12,13 +12,13 @@ source env/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 
-sed -i "s~dbms_template_path~$project_path~g" beget/site.conf
-sed -i "s~dbms_template_domain~$project_domain~g" beget/site.conf drf2/settings.py
+sed -i "s~dbms_template_path~$project_path~g" .beget/site.conf
+sed -i "s~dbms_template_domain~$project_domain~g" .beget/site.conf drf2/settings.py
 
-sudo ln -s $project_path/beget/site.conf /etc/nginx/sites-enabled/
-sudo ln -s $project_path/beget/service /etc/systemd/system/
+ln -s $project_path/.beget/site.conf /etc/nginx/sites-enabled/
+ln -s $project_path/.beget/service /etc/systemd/system/
 
-sudo systemctl daemon-reload
-sudo systemctl start beget
-sudo systemctl enable beget
-sudo service nginx restart
+systemctl daemon-reload
+systemctl start beget
+systemctl enable beget
+service nginx restart
